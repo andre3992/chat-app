12fac0c08dbf6c9035524541186090340d8e9525
andre3992 <andre.regedor@hotmail.com>
2020-01-18 14:09:49 +0000

    change status

--- src/components/Contactlist.js
+++ src/components/Contactlist.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import Contact from "./contact"
+import React from "react";
+import Contact from "./contact";
 
-
-function ContactList () {
+class ContactList extends React.Component {
+  render() {
     const names = [
-    { 
-      avatar:"https://randomuser.me/api/portraits/men/54.jpg",
-      name:"Robert",
-      status:true
-    },
+      {
+        avatar: "https://randomuser.me/api/portraits/men/54.jpg",
+        name: "Robert",
+        status: true
+      },
 
-    {
-      avatar:"https://randomuser.me/api/portraits/women/76.jpg",
-      name:"Julia",
-      status:true
-    },
+      {
+        avatar: "https://randomuser.me/api/portraits/women/76.jpg",
+        name: "Julia",
+        status: false
+      },
 
-    {
-      avatar:"https://randomuser.me/api/portraits/women/23.jpg",
-      name:"Jane",
-      status:false
-    },
-      
-    ]
+      {
+        avatar: "https://randomuser.me/api/portraits/women/23.jpg",
+        name: "Jane",
+        status: true
+      }
+    ];
 
     return (
       <ul>
-        {
-         names.map(item => 
-        <Contact name = {item.name} avatar = {item.avatar} status = {item.status} />)
-        }
+        {names.map(item => (
+          <Contact key={item.name} name={item.name} avatar={item.avatar} status={item.status} />
+        ))}
       </ul>
     );
   }
+}
 
 export default ContactList;
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import "./contact.css"
+import React from "react";
+import "./contact.css";
 
 
-
-function Contact(props) {
-    return ( <figure className = "Contact" >
-        
-        <img src={props.avatar}
-        alt={props.character}
-        /> 
+class Contact extends React.Component { 
+  constructor(props){
+      super(props);
+      this.state={
+          online:this.props.status
+      }
+  }
+  render() {
+    return (
+      <figure className="Contact" onClick={()=>{
+          this.setState({online:!this.state.online})
+      }}>
+        <img src={this.props.avatar} alt={this.props.character} />
         <div>
-        <div className="name"> {props.name} </div> 
-        <div className="status"> {
-            props.status ? <div className="status-online">
-            <p> online </p> </div> : <div className="status-offline"> 
-            <p> offilne </p> 
-        </div> 
-        } 
-        </div> 
-        </div> 
-        </figure>
-    )
+          <div className="name"> {this.props.name} </div>
+          <div className="status">
+            {this.state.online ? (
+              <div className="status-online" >
+                <p> online </p>
+              </div>
+            ) : (
+              <div className="status-offline">
+                <p> offline </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </figure>
+    );
+  }
 }
-
 
 export default Contact;
